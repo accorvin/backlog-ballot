a277a840a1cbf23da1f388f3c2176e241a20b828
Alex Corvin <acorvin@redhat.com>
2018-06-26 17:13:35 -0400

    Fix frontend environments path

--- frontend/src/app/issue.service.ts
+++ frontend/src/app/issue.service.ts
@@ -4,7 +4,7 @@ import { Observable, of } from 'rxjs';
 import { catchError, map, tap } from 'rxjs/operators';
 
 import { Issue } from './issue';
-import { environment } from '../../environments/environment';
+import { environment } from '../environments/environment';
 
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
