d7fbfddc8d3d0a14dad78f6c806fb5c31cf95690
Alex Corvin <acorvin@redhat.com>
2018-06-26 19:45:19 -0400

    Fix production backend api url

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  api: 'https://https://backend-backlog-ballot.192.168.64.3.nip.io/api/'
+  api: 'https://backend-backlog-ballot.192.168.64.3.nip.io/api/'
 };
